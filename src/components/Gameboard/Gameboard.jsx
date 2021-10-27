557fa80c6054ae974b0b0f4cf98a1b7f0415bc77
Lukas Zöllner <lks.zoellner@gmail.com>
2021-10-27 16:50:22 +0200

    started working on the game logic

---
+++ src/components/Gameboard/Gameboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Gameboard = (props) => {
+  return <div></div>;
+};
+
+Gameboard.propTypes = {};
+
+Gameboard.defaultProps = {};
+
+export default Gameboard;
