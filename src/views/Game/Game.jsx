5ac0c175fe4335415ca4dbd8e8476aa2e3502653
Stephan Lüddemann <Stephan_Lueddemann@web.de>
2021-11-01 15:22:26 +0100

    Added reset function on gameover and on reset in menu.Removed opacity from modal, because the whole modal was transparent. (#15)

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -3,13 +3,55 @@ import { useHistory } from "react-router";
 import { Menu } from "../../components/Menu/Menu";
 import { Gameboard } from "../../components/Gameboard/Gameboard";
 import { Modal } from "../../components/Modal/Modal";
+import { Button } from "../../components/Button/Button";
+import { CardHiCF } from "../../components/CardHiCF/CardHiCF";
 import { getMockData } from "../../services/api.services";
+import cover from "../../assets/Cover.jpg";
 
 const data = getMockData().slice(0, 2);
 
 export function Game({ ...props }) {
   const history = useHistory();
   const [gameOver, setGameOver] = useState(false);
+  const [gameCount, setGameCount] = useState(1);
+
+  const resetGame = function () {
+    setGameCount((last) => last + 1);
+    setGameOver(false);
+  };
+
+  const gameOverCard = {
+    img: {
+      src: cover,
+      alt: "Rick & Morty",
+    },
+    content: (
+      <div className="p-4 pt-3 flex flex-col gap-4">
+        <p
+          className="font-black text-primary uppercase"
+          style={{ fontSize: "2rem" }}
+        >
+          🎉 Congratulation 🎉
+        </p>
+        <p
+          className="font-black text-primary uppercase"
+          style={{ fontSize: "2rem" }}
+        >
+          you finished the game!
+        </p>
+      </div>
+    ),
+    footer: (
+      <div>
+        <Button
+          label="PLAY AGAIN"
+          onClick={() => {
+            resetGame();
+          }}
+        />
+      </div>
+    ),
+  };
 
   return (
     <div
@@ -19,7 +61,7 @@ export function Game({ ...props }) {
       <div className="absolute top-0 w-full">
         <Menu
           onCancel={() => history.push("/")}
-          onReset={() => console.log("reset")}
+          onReset={() => resetGame()}
           initiallyOpen={false}
         />
       </div>
@@ -30,11 +72,18 @@ export function Game({ ...props }) {
           onGameOver={() => {
             setGameOver(true);
           }}
+          key={gameCount}
         />
 
         {gameOver && (
           <Modal>
-            <h1>GAME OVER </h1>
+            <div className="w-1/2 opacity-100">
+              <CardHiCF
+                img={gameOverCard.img}
+                content={gameOverCard.content}
+                footer={gameOverCard.footer}
+              />
+            </div>
           </Modal>
         )}
       </div>
