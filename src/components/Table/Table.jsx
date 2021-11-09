241674ef546e710202e316be17c15f725bc08a4a
Lukas <lks.zoellner@gmail.com>
2021-11-09 16:03:11 +0100

    added table component (#26)
    * added table component
    
    Takes a headers array and a rows array (of arrays) and generates a minimally styled table.
    
    * added storybook testcase for custom classes
    
    * className is now extensible
    
    You can now add additional classes to the component just by using the className prop as per usual.
    Also removed files i accidentally added on a prior commit

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Table = ({ headers, rows, className, ...props }) => {
+  return (
+    <table className={className ? className + " table" : "table"} {...props}>
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => (
+          <tr>
+            {row.map((cell) => (
+              <td>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+Table.propTypes = {
+  /**
+   * Headers
+   */
+  headers: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Rows
+   */
+  rows: PropTypes.arrayOf(PropTypes.array),
+};
