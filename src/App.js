73972ee5c6d124a8f1f27f8e50e8456f25509f90
Stephan Lüddemann <Stephan_Lueddemann@web.de>
2021-11-09 16:52:30 +0100

    Feature/online players (#31)
    * Added player.service and player hook. Hook does not work right now in app.js. The login will push the user into players in the db. On logout the player will be removed
    
    * If user was logged in but never logged out and the user logs in again, the userPlayer will be set accordingly
    
    * Renamed userPlayer into userPlayerRef
    
    * Refactoring. Put the setPlayerOnline to the players.service instead of having it in the app.js.
    
    * Refactored accordingly to the code review session.
    
    * Resolved code review suggestions.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./styles/App.css";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 import { Home } from "./views/Home/Home";
 import { Game } from "./views/Game/Game";
 import { Login } from "./components/Login/Login";
 import { Menu } from "./components/Menu/Menu";
 import { Button } from "./components/Button/Button";
-import { useState } from "react";
 import { useFirebaseApp, DatabaseProvider, useUser } from "reactfire";
 import { getDatabase } from "firebase/database"; // Firebase v9+
-
+import {
+  setPlayerOnline,
+  removePlayerOnline,
+} from "./services/player.service.mjs";
+import { register, login, logout } from "./services/auth.service.mjs";
 function App() {
   const app = useFirebaseApp();
   const database = getDatabase(app);
   const auth = getAuth();
-  const { data: user } = useUser();
+  const { data: loggedinUser } = useUser();
   const [loadingLogin, setLoadingLogin] = useState(false);
   const [loginError, setLoginError] = useState("");
 
@@ -26,11 +26,13 @@ function App() {
     try {
       setLoginError("");
       setLoadingLogin(true);
-      await createUserWithEmailAndPassword(auth, user.email, user.password);
+      await register(auth, user);
       setLoadingLogin(false);
     } catch (error) {
       setLoadingLogin(false);
       setLoginError(error.message);
+      console.log(error);
+      console.log(error.message);
     }
   };
 
@@ -38,25 +40,31 @@ function App() {
     try {
       setLoginError("");
       setLoadingLogin(true);
-      await signInWithEmailAndPassword(auth, user.email, user.password);
+      await login(auth, user);
       setLoadingLogin(false);
     } catch (error) {
       setLoadingLogin(false);
       setLoginError(error.message);
+      console.log(error);
+      console.log(error.message);
     }
+  };
+
+  const handleLogout = async () => {
+    await logout(auth, loggedinUser);
   };
 
   return (
     <DatabaseProvider sdk={database}>
       <div className="App">
-        {user ? (
+        {loggedinUser ? (
           <div className="flex flex-col gap-4 items-center">
             <Menu initiallyOpen={false}>
               <Button
                 label="LOGOUT"
                 variant="secondary"
                 onClick={() => {
-                  auth.signOut();
+                  handleLogout();
                 }}
               />
             </Menu>
