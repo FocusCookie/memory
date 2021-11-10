8a1a2f41297469376bc80e438762cf1de0766204
FocusCookie <stephan_lueddemann@web.de>
2021-11-10 10:09:16 +0100

    Removed menu from app js and inlude it in home view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./styles/App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { getAuth } from "firebase/auth";
 import { Home } from "./views/Home/Home";
 import { OfflineGame } from "./views/OfflineGame/OfflineGame";
 import { OnlineGame } from "./views/OnlineGame/OnlineGame";
 import { Login } from "./components/Login/Login";
-import { Menu } from "./components/Menu/Menu";
-import { Button } from "./components/Button/Button";
 import { useFirebaseApp, DatabaseProvider, useUser } from "reactfire";
 import { getDatabase } from "firebase/database"; // Firebase v9+
 import { register, login, logout } from "./services/auth.service.mjs";
@@ -15,7 +12,6 @@ import { register, login, logout } from "./services/auth.service.mjs";
 function App() {
   const app = useFirebaseApp();
   const database = getDatabase(app);
-  const auth = getAuth();
   const { data: loggedinUser } = useUser();
   const [loadingLogin, setLoadingLogin] = useState(false);
   const [loginError, setLoginError] = useState("");
@@ -57,15 +53,6 @@ function App() {
       <div className="App">
         {loggedinUser ? (
           <div className="flex flex-col gap-4 items-center">
-            <Menu initiallyOpen={false}>
-              <Button
-                label="LOGOUT"
-                variant="secondary"
-                onClick={() => {
-                  handleLogout();
-                }}
-              />
-            </Menu>
             <Router>
               <Switch>
                 <Route exact path="/">
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,15 +4,29 @@ import { Button } from "../../components/Button/Button";
 import { Menu } from "../../components/Menu/Menu";
 import { useHistory } from "react-router-dom";
 import coverImg from "../../assets/Cover.jpg";
+import { logout } from "../../services/auth.service.mjs";
 
 export function Home({ ...props }) {
   const history = useHistory();
+
+  const handleLogout = async () => {
+    await logout();
+  };
 
   return (
     <div
       {...props}
       className="flex flex-col gap-8 justify-center items-center  bg-greyscale-offWhite"
     >
+      <Menu initiallyOpen={false}>
+        <Button
+          label="LOGOUT"
+          variant="secondary"
+          onClick={() => {
+            handleLogout();
+          }}
+        />
+      </Menu>
       <Card>
         <img src={coverImg} alt="Rick and Morty" style={{ width: "27rem" }} />
         <div className="p-4 pt-3 flex flex-col gap-4">
